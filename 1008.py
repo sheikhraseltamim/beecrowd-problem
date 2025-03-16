d1281e6f57aac20a71535e591184bce210679ac7
sheikhraseltamim <sheikhraseltamim@gmail.com>
2025-03-16 23:31:22 +0600

    many q: and Ans:

---
+++ 1008.py
@@ -0,0 +1,6 @@
+employees_number = int(input())
+employees_hourly = int(input())
+employees_hour = float(input())
+employees_salary = employees_hourly * employees_hour
+print(f"NUMBER = {employees_number}")
+print(f"SALARY = U$ {employees_salary:.2f}")
